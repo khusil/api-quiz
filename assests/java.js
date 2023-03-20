f52436d8cd64200b72f1cc5174f22664a99fcd91
khusil <khushilmanji@yahoo.com>
2023-03-20 12:31:53 -0400

    added var of questions

---
+++ assests/java.js
@@ -0,0 +1,44 @@
+var quiz [
+    {
+        question: " commonly used data types Do Not include",
+        answers:[
+            "A string",
+            "boolean",
+            "alerts",
+            "numbers"
+        ],
+        correctanswer: "alerts"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with?",
+        answers:[
+            "Quotes",
+            "curly brackets",
+            "parenthesis",
+            "Square brackets"
+        ],
+        correctanswer: "curly brackets"   
+            
+    },
+    {
+        question: "Arrays in javascript can be used to store?",
+        answers:[
+            "Number os strings",
+            "other arrays",
+            "booleans",
+            "All the above"
+        ],
+        correctanswer: "All the above"
+    },
+    {
+        question:"a very useful tool used during development and debugging for printing content to the debugger is",
+        answers:[
+            "javascript",
+            "terminal/bash",
+            "for loops",
+            "console logs"
+        ],
+        correctanswer:"javascript"
+    },
+    
+]
